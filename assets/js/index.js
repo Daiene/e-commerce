0cb67a69d9c13d7481605da2a8dc6fe64e2ee747
Daiene Batagioti <daiene.montuani@hotmail.com>
2023-02-02 23:28:07 -0300

    Putting header information

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+
+
+header.insertAdjacentHTML("beforeend", `
+    <div class="header">
+        <h1>Fael Hamburguer</h1>
+        <a href="">Início</a>
+        <a href="">Faça seu Pedido</a>
+        <a href="">Nossos Princípios</a>
+        <a href="">Fale Conosco</a>
+    </div>
+`)
+
+main.insertAdjacentHTML("beforeend", `
+    <div class="conteudo">
+        <h1>Olá</h1>
+    </div>
+`)
